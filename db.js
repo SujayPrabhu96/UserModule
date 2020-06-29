00a38f73c3d07fc16ac6548728ee7ed5afd77ae0
SujayPrabhu96 <prabhusujay@gmail.com>
2020-06-29 21:04:24 +0530

    dotenv variables

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
+require('dotenv').config()
 const Sequelize = require('sequelize');
 const db = {};
 
-const sequelize = new Sequelize('user_module','root','',{
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME,process.env.DB_PASSWORD,{
+    host: process.env.DB_HOST,
     dialect: 'mysql',
-    // dialectModule: "mysql2",
     pool: {
       max: 10,
       min: 0,
